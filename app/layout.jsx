78b21314a920625c9265f6cd8d6cc657e8687a03
Ferris Chang <103004499+PandaFChangs@users.noreply.github.com>
2023-06-22 10:05:44 -0400

    tab changed

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from '@components/Nav'
 import Provider from '@components/Provider'
 
 export const metadata = {
-  title: "Promptopia",
+  title: "PromptMe",
   description: 'Discover & Share AI Prompts'
 }
 const RootLayout = ({children}) => {
