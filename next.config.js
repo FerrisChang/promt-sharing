8d4171950e8ad4df9770497facad760dfb25adcd
Ferris Chang <103004499+PandaFChangs@users.noreply.github.com>
2025-04-21 12:44:19 -0400

    chore: update Next.js to v14.2.28 and optimize configuration

--- next.config.js
+++ next.config.js
@@ -1,19 +1,47 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
-  },
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['avatars.githubusercontent.com'],
+    formats: ['image/avif', 'image/webp'],
   },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
+  experimental: {
+    optimizeCss: true,
+    serverActions: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 70000,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            priority: -10,
+            reuseExistingChunk: true,
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      }
     }
     return config
-  }
+  },
 }
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer(nextConfig)
